e81163a6f9f1b805c34723bd4c038e20a93a2bc2
Dijair Camargo <dijair.camargo@gmail.com>
2024-05-15 12:12:07 -0300

    Initial commit

---
+++ src/br/com/SintaxeJavaBasico/AnatomiaDasClasses/Aula001.java
@@ -0,0 +1,29 @@
+package br.com.SintaxeJava.AnatomiaDasClasses;
+
+public class Aula001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        double num1 = 14.31;
+        double num2 = 13.50;
+        double num3 = 13.41;
+        double num4 = 7.32;
+        double num5 = 10.42;
+        double num6 = 8.31;
+
+        double soma = num1 + num2 + num3 +
+                num4 + num5 + num6;
+
+        System.out.println("Soma: " + soma);
+
+        String primeiroNome = "Junior";
+        String segundoNome = "Camargo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
---
+++ src/br/com/SintaxeJavaBasico/AnatomiaDasClasses/Aula002.java
@@ -0,0 +1,17 @@
+package br.com.SintaxeJava.AnatomiaDasClasses;
+
+public class Aula002 {
+    public static void main(String[] args) {
+
+        double mediaFinal = 8;
+
+        if(mediaFinal < 6)
+            System.out.println("REPROVADO");
+
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+
+        else
+            System.out.println("APROVADO");
+    }
+}
