d0923319bf04a7e102d7c21fbdfd0fae171bd3d5
Dijair Camargo <dijair.camargo@gmail.com>
2024-05-22 10:27:20 -0300

    update

---
+++ src/br/com/SintaxeJavaBasico/TerminalArgumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package br.com.SintaxeJavaBasico.TerminalArgumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal!");
+    }
+}
