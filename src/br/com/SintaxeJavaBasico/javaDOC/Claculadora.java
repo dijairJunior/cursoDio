9b07da2212a3165f537004dcd739d1ffa5bc6245
Dijair Camargo <dijair.camargo@gmail.com>
2024-05-22 09:51:41 -0300

    add file

---
+++ src/br/com/SintaxeJavaBasico/javaDOC/Claculadora.java
@@ -0,0 +1,8 @@
+package br.com.SintaxeJavaBasico.javaDOC;
+
+public class Claculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/br/com/SintaxeJavaBasico/javaDOC/Comentarios.java
@@ -0,0 +1,22 @@
+package br.com.SintaxeJavaBasico.javaDOC;
+
+public class Comentarios {
+
+    /* Este método foi elaborado as pressas
+     * por isso eu abreviei o nome das variaveis
+     * mas olha, ele tem a finalidade somar ou multiplicar
+     * dois numeros.
+     */
+
+    public static void main(String[] args) {
+        /*int r = 0; // r é igual ao resultado
+        if(m == "M"){ // M = multiplicação
+            r = n * x;
+        } else {
+            // se não soma mesmo
+            r = n * x;
+        }
+        return r;
+         */
+    }
+}
